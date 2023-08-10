bf9b42ba09ee47f40684223ba31a02f0ad7f7578
acallvazq <runo-mola12@hotmail.es>
2023-08-10 09:04:26 +0200

    filtro combo no funciona

--- frontend/bankmanager-web~/src/app/main/travels/travels-table/travels-table.component.ts
+++ frontend/bankmanager-web~/src/app/main/travels/travels-table/travels-table.component.ts
@@ -8,16 +8,15 @@ import { FilterExpressionUtils } from 'ontimize-web-ngx';
   styleUrls: ['./travels-table.component.css']
 })
 export class TravelsTableComponent implements OnInit {
-
   stock:number;  
   constructor() { }
 
   ngOnInit() {
   }
 
-  createFilter(values: Array<{attr,value}>): Expression{
+  createFilters(values: Array<{attr,value}>): Expression{
     let filters = [];
-
+    console.log(values);
     values.forEach(fila => {
       if(fila.value){
         if(fila.attr === "STARTDATE_I"){
@@ -31,16 +30,37 @@ export class TravelsTableComponent implements OnInit {
         
         }if(fila.attr === "ENDDATE_E"){
           filters.push(FilterExpressionUtils.buildExpressionLessEqual("datetime_out", fila.value));
-        }
         
+        }if(fila.attr === "PLATENUMBER"){
+          filters.push(FilterExpressionUtils.buildExpressionEquals("plate_number", fila.value));
+          
+        }
       }
     });
 
     if(filters.length > 0){
-      return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
-    
+      let expression = filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND)); 
+      console.log(expression);
+      return expression;
     }else return null;
   
   }
+
+  reloadAll(){
+    console.log("hola");
+    window.location.reload();
+  }
+  /*filterByPlates(values){
+    let filters = [];
+    let plates = values.attr
+    if(plates){
+      plates.forEach( plate => {
+        filters.push(FilterExpressionUtils.buildExpressionEquals("plate_number", plate));
+      });
+    }
+    if(filters.length > 0){
+      return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_OR));
+    }else return null;
+  }*/
 
 }
