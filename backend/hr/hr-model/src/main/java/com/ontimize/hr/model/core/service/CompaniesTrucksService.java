5178240254fa33314d5193d1d2c78d5fa5e794e6
Sergio <sergiobouzasantas@gmail.com>
2023-08-28 14:44:51 +0200

    Añadí CompaniesTruck

---
+++ backend/hr/hr-model/src/main/java/com/ontimize/hr/model/core/service/CompaniesTrucksService.java
@@ -0,0 +1,45 @@
+package com.ontimize.hr.model.core.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ontimize.hr.api.core.service.ICompaniesTrucksService;
+import com.ontimize.hr.model.core.dao.CompaniesTrucksDao;
+import com.ontimize.hr.model.core.dao.TravelsDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+
+@Service("CompaniesTrucksService")
+@Lazy
+public class CompaniesTrucksService implements ICompaniesTrucksService {
+    @Autowired private CompaniesTrucksDao companiesTrucksDao;
+    @Autowired private DefaultOntimizeDaoHelper daoHelper;
+
+    @Override
+    public EntityResult companyTrucksQuery(Map<String, Object> keyMap, List<String> attrList)
+            throws OntimizeJEERuntimeException{
+            return this.daoHelper.query(this.companiesTrucksDao, keyMap, attrList);
+    }
+
+    @Override
+    public EntityResult companyTrucksInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+        return this.daoHelper.insert(this.companiesTrucksDao, attrMap);
+    }
+
+    @Override
+    public EntityResult companyTrucksUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+            throws OntimizeJEERuntimeException {
+        return null;
+    }
+
+    @Override
+    public EntityResult companyTrucksDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+        return null;
+    }
+
+}
